c02957052e4cc80e1920101385129fbfce4b4aca
m4l1c3 <jon@jonsomnia.com>
2017-09-26 11:43:55 -0600

    Initial commit

---
+++ Examples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Examples.Startup))]
+namespace Examples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
